8b81c5af25e842f705c8f4992d8fc5de202414f4
SamVsCode <samvscode@gmail.com>
2018-01-07 03:03:34 +0530

    added seed data

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,7 +9,7 @@
  * https://sailsjs.com/config/bootstrap
  */
 
-module.exports.bootstrap = async function(done) {
+module.exports.bootstrap = async function (done) {
 
   // By convention, this is a good place to set up fake data during development.
   //
@@ -30,8 +30,46 @@ module.exports.bootstrap = async function(done) {
   // Don't forget to trigger `done()` when this bootstrap function's logic is finished.
   // (otherwise your server will never lift, since it's waiting on the bootstrap)
 
+  if (await Service.count() > 0) {
+    console.log("Services are already in db");
+  } else {
+    await Service.createEach([{ "createdAt": 1514124380, "updatedAt": 1514124380, "id": 1, "name": "Normal Car Wash" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 2, "name": "Platinum Car Wash" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 3, "name": "Platinum ECI" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 4, "name": "Gold ECI" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 5, "name": "Platinum Organic Coating" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 6, "name": "Gold Organic Coating" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 7, "name": "AC Vents Sterilization" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 8, "name": "Engine Dust Protection" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 9, "name": "Windshield Treatment" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 10, "name": "Metal Wheel Polish" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 11, "name": "Chaises Rubber Coating" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 12, "name": "K3 Service Pro Treatment" },
+    { "createdAt": 1514130453, "updatedAt": 1514130453, "id": 13, "name": "Brand Logo Beautification" },
+    { "createdAt": 1514130453, "updatedAt": 1514130453, "id": 14, "name": "Headlight Restoration" }]);
 
+    console.log("seed: created all services");
+  }
+  if (await Product.count() > 0) {
+    console.log("Products are already in db");
+  } else {
+    await Product.createEach([{ "createdAt": 1514124380, "updatedAt": 1514124380, "id": 1, "name": "Cream wax" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 2, "name": "Microfiber Cloth" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 3, "name": "Glass Cleaner" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 4, "name": "Dashboard Dresser" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 5, "name": "Car Wash Shampoo" },
+    { "createdAt": 1514124380, "updatedAt": 1514124380, "id": 6, "name": "Tyre Dresser" }]);
+    console.log("seed: created all products");
+  }
 
+  if (await Admin.count() > 0) {
+    console.log("Admin credentials already in db");
+  } else {
+    await Admin.create(
+      {"createdAt":1514062923558,"updatedAt":1514062923558,"id":2,"username":"root","password":"root"}
+    );
+    console.log("seed: created the admin credentials");
+  }
   return done();
 
 };
