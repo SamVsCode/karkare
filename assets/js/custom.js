623f2feb7941c45a781d66e43ffb258976ad2bb4
SamVsCode <samvscode@gmail.com>
2018-01-07 01:52:28 +0530

    new feature, filter by date range

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,6 +33,28 @@
 // }());
 
 $(document).ready(function () {
+
+    $.fn.dataTableExt.afnFiltering.push(
+        function (oSettings, aData, iDataIndex) {
+            var minDate = $("#min").val();
+            var maxDate = $("#max").val();
+            var date = new Date(aData[3]).getTime();
+            if(minDate!== "" && minDate !== undefined && minDate !== "undefined" &&
+                maxDate!== "" && maxDate !== undefined && maxDate !== "undefined"){
+                    var minTime = new Date(minDate).getTime();
+                    var maxTime = new Date(maxDate).getTime();     
+                    if(date >= minTime && date <= maxTime){
+                        return true;
+                    }else{
+                        return false
+                    }
+                }else{
+                    return true
+                }
+        }
+    );
+
+
     // Setup - add a text input to each footer cell
     $('#dataTable tfoot th').each(function () {
         var title = $(this).text();
@@ -43,9 +65,16 @@ $(document).ready(function () {
     var datatable = $('#dataTable').DataTable({
         "scrollX": true,
         "columnDefs": [
-            {"width": "50px", "targets":0}
+            { "width": "50px", "targets": 0 }
         ],
-        buttons: [ 'copy', 'csv', 'excel' ]
+        buttons: ['copy', 'csv', 'excel']
+    });
+
+    $("#min").on('change', function(){
+        datatable.draw();
+    });
+    $("#max").on('change', function(){
+        datatable.draw();
     });
 
     // Apply the search
